bcaf8a8bd88f93684333b17e789e67175196ea12
Thành Danh <danh.nguyen1507@hcmut.edu.vn>
2026-04-11 11:19:51 +0700

    first procedure

--- procedure.sql
+++ procedure.sql
@@ -1,12 +1,41 @@
+DELIMITER $$
 
+CREATE PROCEDURE CHECK_VEHICLE_PLATE (
+    IN p_plate_number VARCHAR(20),
+    IN p_make VARCHAR(20),
+    IN p_model VARCHAR(20),
+    IN p_color VARCHAR(10),
+    IN p_capacity INT,
+    IN p_registrant_id INT,
+    IN p_using_driver_id INT
+)
+BEGIN
+    -- Check plate format
+    IF p_plate_number NOT REGEXP '^[0-9]{2}[A-Z]{1,3}-[0-9]{3}\\.[0-9]{2}$' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid plate number format';
+    END IF;
 
+    -- Insert
+    INSERT INTO VEHICLE (
+        PLATE_NUMBER,
+        MAKE,
+        MODEL,
+        COLOR,
+        CAPACITY,
+        REGISTRANT_ID,
+        USING_DRIVER_ID
+    )
+    VALUES (
+        p_plate_number,
+        p_make,
+        p_model,
+        p_color,
+        p_capacity,
+        p_registrant_id,
+        p_using_driver_id
+    );
 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
--- CALL procedure join bảng  hoặc procedure tính toán phức tạp cho API 
+END$$
+
+DELIMITER ;
